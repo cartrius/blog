08a9119061b726864de576beeb325b5c0a81c3dd
Cartrius Phipps <cartriusphipps@gmail.com>
2026-03-27 12:06:49 -0500

    Restrict origins to blog-reader and blog-auto vercel URLs

--- blog-api/app.js
+++ blog-api/app.js
@@ -8,7 +8,13 @@ const commentRouter = require("./routes/commentRouter");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: [
+        "https://blog-reader.vercel.app",
+        "https://blog-author.vercel.app"
+    ]
+}));
+
 app.use(express.json());
 
 app.use("/api/auth", authRouter);
